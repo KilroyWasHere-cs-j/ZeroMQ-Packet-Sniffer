ab285bcb5c3b08e94d7f07d5d2339b98ff843ae6
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2023-01-20 22:02:25 -0500

    Working

---
+++ ZeroMQ_Sniffer/main.py
@@ -0,0 +1,38 @@
+import cv2
+import imagezmq
+import datetime
+import time
+
+
+def getTime():
+    return datetime.datetime.now()
+
+
+def stream():
+    image_hub = imagezmq.ImageHub()
+    print("Reading in video")
+    print("Kilroy Was Here")
+    while True:  # show streamed images until Ctrl-C
+        rpi_name, image = image_hub.recv_image()
+
+        # font which we will be using to display FPS
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+        cv2.imshow(rpi_name, image)  # 1 window for each RPi
+
+        # used to record the time when we processed last frame
+        # we will be subtracting it to get more accurate result
+        print("Frame at {0}".format(getTime()))
+
+        cv2.waitKey(1)
+
+        image_hub.send_reply(b'OK')
+
+def main():
+    print("Ready to handle video")
+    stream()
+
+
+if __name__ == "__main__":
+    main()
